d7eb9711f2e409dad39d945e82450cdfc78abac8
Naveen Kumar <iit2019066@iiita.ac.in>
2022-03-02 20:52:07 +0530

    Done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const gridDisplay = document.querySelector('#grid')
-const score = document.querySelector('#score') 
+const resultDisplay = document.querySelector('#score') 
 
-const card = [
+const cardArray = [
     {
         name: 'fries',
         img: 'images/fries.png'
@@ -51,3 +51,60 @@ const card = [
         img: 'images/hotdog.png'
       }
 ]
+
+let cardChosen = []
+let cardChosenId =[]
+let cardsWon = []
+cardArray.sort(()=> 0.5-Math.random())
+
+createBoard()
+
+function createBoard(){
+  for(let i=0;i<cardArray.length;i++){
+    let card = document.createElement('img')
+    card.setAttribute('src','images/blank.png')
+    card.setAttribute('data-id',i);
+    card.addEventListener('click',flipped)
+    gridDisplay.appendChild(card)
+  }
+}
+
+function check() {
+  const cards = document.querySelectorAll('img')
+    const optionOneId = cardChosenId[0]
+    const optionTwoId = cardChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('You have clicked the same image!')
+    }
+    else if (cardChosen[0] === cardChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipped)
+      cards[optionTwoId].removeEventListener('click', flipped)
+      cardsWon.push(cardChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardChosen = []
+    cardChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+}
+function flipped(){
+  const cardId = this.getAttribute('data-id')
+  cardChosen.push(cardArray[cardId].name)
+  cardChosenId.push(cardId)
+  this.setAttribute('src',cardArray[cardId].img)
+
+  if(cardChosen.length === 2){
+    setTimeout(check,500);
+  }
+}
